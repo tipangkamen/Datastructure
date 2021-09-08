2e969237ad7dc9fadd3d0a6e7aa7feb6798733be
zhuguancheng <1005071600@qq.com>
2021-09-08 21:15:38 +0800

    Initial commit

---
+++ src/main/java/com/xiaozhu/algorithm/SparseArray.java
@@ -0,0 +1,11 @@
+package com.xiaozhu.algorithm;
+
+public class SparseArray {
+    static int a;
+    static int b;
+    public static void main(String[] args) {
+        //创建一个原始的二维数组
+        int[][] sparse = new int[a][b];
+        
+    }
+}
