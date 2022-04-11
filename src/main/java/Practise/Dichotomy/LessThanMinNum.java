5b7a137419d3507c5e8d42ce14241a2e03baa424
zhuguancheng <1005071600@qq.com>
2022-04-12 00:37:19 +0800

    <feat>:二分法

--- src/main/java/Practise/Dichotomy/LessThanMinNum.java
+++ src/main/java/Practise/Dichotomy/LessThanMinNum.java
@@ -1,22 +1,32 @@
 package Practise.Dichotomy;
 
 /*
-*   一个有序数组，给出一个数字,需要找到小于这个数字的数字
-* */
+ *   一个有序数组，给出一个数字,需要找到小于这个数字的数字中的最右边的下标
+ * */
 public class LessThanMinNum {
 
-    public static int findLessThanMinNum(int[] arr,int minNum){
-        int l=0;
-        int r=arr.length-1;
-
-        while (l<r){
-            int index=l+(r-l)>>2;
-            if(arr[index]>=minNum){
-                r=index;
+    public static int findLessThanMinNum(int[] arr, int minNum) {
+        int l = 0;
+        int r = arr.length - 1;
+        int index = -1;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (arr[mid] >= minNum) {
+                index = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
-
-
         }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 3, 4, 5, 6, 6, 9};
+        int[] arr1 = {1, 1, 3, 4, 5, 6, 6, 9};
+        System.out.println(findLessThanMinNum(arr, 3));
+        System.out.println(findLessThanMinNum(arr1, 3));
 
     }
+
 }
--- src/main/java/Practise/Dichotomy/MinInterval.java
+++ src/main/java/Practise/Dichotomy/MinInterval.java
@@ -1,47 +1,40 @@
 package Practise.Dichotomy;
 
 /*
-*  一个无序数组  如果[0]<[1] 则[0]是区间最小数  [length-2]>[length-1]则[length-1]是区间最小数 其他的数需要满足 [n-1]>[n]<[n+1] 则[n]是区间最小数
-* 现在只需要返回任意一个区间最小数
-* */
+ *  一个无序数组(任意两个相邻的数不相等)  如果[0]<[1] 则[0]是区间最小数  [length-2]>[length-1]则[length-1]是区间最小数 其他的数需要满足 [n-1]>[n]<[n+1] 则[n]是区间最小数
+ * 现在只需要返回任意一个区间最小数的下标
+ * */
 public class MinInterval {
 
 
-    private static int findMinIntervalByDichotomy(int[] arr){
-        int l=0;
-        int r=arr.length-1;
-
-        while (l<=r){
-
-            /*
-                不用这两个是因为 1可能超过int的最大长度  2的除法比加法慢，比位运算更慢
-                int index=(l+r)/2;
-                int index=l/2+r/2;
-             */
-
-            int index=l+(r-l)>>2;
-            if (index==0 && arr[l]<arr[index]){
-                return arr[index];
-            }else if (index==arr.length-1 && arr[arr.length-2]>arr[arr.length-1]){
-                return arr[index];
-            }else {
-                if (arr[index-1]>arr[index] && arr[index]<arr[index+1]){
-                    return arr[index];
-                }
-            }
-
-
+    private static int findMinIntervalByDichotomy(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
         }
+        if (arr[0] < arr[1]) {
+            return arr[0];
+        }
+        if (arr[arr.length - 2] > arr[arr.length - 1]) {
+            return arr[arr.length - 1];
+        }
+        int l = 1;
+        int r = arr.length - 2;
+        int mid;
+        while (l < r) {
+            mid = l + ((r - l) >> 1);
+            if (arr[mid] > arr[mid - 1]) {
+                r = mid - 1;
+            } else if (arr[mid] > arr[mid + 1]) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return l;
 
-
-
-
-
-        return 0;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
-    }
 
 }
+
+
